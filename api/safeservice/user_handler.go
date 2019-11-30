4d841fd528395eeb54a091713ea59b0967453647
王梓逻 <779158078@qq.com>
2019-11-30 23:24:28 +0800

    format

--- api/safeservice/user_handler.go
+++ api/safeservice/user_handler.go
@@ -124,7 +124,7 @@ func addRelationship(c *gin.Context) {
 
 // getRelationship 获取用户亲友关系接口
 func getRelationship(c *gin.Context) {
-	userID, err := util.ParseUint(c.Query("userID"))
+	userID, _ := util.ParseUint(c.Query("userID"))
 	if userID == 0 {
 		fail(c, fmt.Errorf("请传入用户ID"))
 		return
